09edfb5ef663986d01a9bd423ef8521b0675a68d
ingchrist <cdjinguet@gmail.com>
2024-02-18 10:26:37 +0100

    files added

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,16 +7,16 @@ env.hosts = ['44.210.150.159', '35.173.47.15']
 env.user = "ubuntu"
 
 
-def do_clean(number=0):
+def wzqdo_clean(wzqnumber=0):
     """ CLEANS """
 
-    number = int(number)
+    wzqnumber = int(wzqnumber)
 
-    if number == 0:
-        number = 2
+    if wzqnumber == 0:
+        wzqnumber = 2
     else:
-        number += 1
+        wzqnumber += 1
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(wzqnumber))
     path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, wzqnumber))
